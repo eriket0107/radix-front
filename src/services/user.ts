12acb3e9c39d334d1a861ce6ccb6fc74aea71b94
Erik <ol.erik0107@gmail.com>
2024-09-28 23:41:02 -0300

    feat: refresh token

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,16 @@
 import { User } from '@/@types'
 import { api } from '@/lib/axios'
+import cookies from 'nookies'
 
 export const fetchUser = async () => {
   const { data } = await api.get<User>('/get-user', {})
+  return data
+}
+
+export const resfreshToken = async () => {
+  const { data } = await api.patch<{ token: string }>('/token/refresh', {})
+
+  api.defaults.headers.common.Authorization = `Bearer ${data.token}`
+  cookies.set(null, 'token', data.token, { path: '/' })
   return data
 }
